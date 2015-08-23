6bab3904222e5c8b6277923d275c80feec1f590b
Basil Siddiqui <basil.siddiqui@gmail.com>
2015-08-23 15:04:21 -0700

    feat(opensesame): initialize app

---
+++ server.js
@@ -0,0 +1,35 @@
+var Hapi = require('hapi')
+var twilio = require('twilio')
+
+var server = new Hapi.Server()
+
+server.connection({
+  port: process.env.PORT || 8080,
+  host: process.env.HOST || '0.0.0.0'
+})
+
+server.route({
+  method: 'POST',
+  path: '/twilio',
+  handler: function (request, reply) {
+    var resp = new twilio.TwimlResponse()
+
+    resp.say('Welcome to Twilio!')
+    .pause({ length: 2 })
+    .say('welcome home', {
+      voice: 'woman',
+      language: 'en-gb'
+    })
+    .play({
+      url: 'http://jetcityorange.com/dtmf/DTMF-9.mp3',
+      loop: 2
+    })
+    .hangup()
+
+    reply(resp.toString())
+  }
+})
+
+server.start(function (err) {
+  console.log('Server started at: ' + server.info.uri)
+})
